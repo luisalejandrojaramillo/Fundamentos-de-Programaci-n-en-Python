a8c620a41b68164260914113042ddbdc2ab050cd
Luis Jaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2019-11-13 22:16:57 -0500

    2. Leccion 2, ejercicio 1

--- Tipos de Datos e Instrucciones Básicas/Leccion2/Ejercicio1.py
+++ Tipos de Datos e Instrucciones Básicas/Leccion2/Ejercicio1.py
@@ -1,9 +1,7 @@
-def main():
-    
+def main():   
     nombreC = input("Digite el nombre de la Criptomoneda: ")
     cantidadC = float(input("Cantidad que tiene de la Criptomoneda: "))
     calorC = float(input("A cuanto equivalr en US$ la Criptomoneda: "))
     res= cantidadC * calorC
     print("Ud. Posee un total de US$ "+str(res))
-
 main()
