936d58c01ca74178602663cb89a2a5bed280b207
Luis Alejandro <luisalejandrojaramillo@hotmail.com>
2019-11-16 17:53:36 -0500

    Estructuras de control

---
+++ 2. Estructuras de control/Leccion1/Ejercicio1.py
@@ -0,0 +1,14 @@
+def main():
+    nom1 = input("Digite el nombre de l criptomoneda 1")
+    can1 = int(input("Digite en numero que tiene de esta"))
+    nom2 = input("Digite el nombre de l criptomoneda 2")
+    can2 = int(input("Digite en numero que tiene de esta"))
+    if can 1 == can2:
+        print(nom1,"y",nom2,"Tienen la misma cantidad, la cual es:",str(can1))
+    elif can1>can2:
+        print(nom1,"Con una cantidad de:",str(can1))
+        print(nom2,"Con una cantidad de:",str(can2))
+    else:
+        print(nom2,"Con una cantidad de:",str(can2))
+        print(nom1,"Con una cantidad de:",str(can1))        
+main()
